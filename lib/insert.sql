9c31275b8e0f8ad61097f91f173a6d98392cebac
Brenden Thornton <thorntonbrenden@gmail.com>
2018-08-10 21:59:31 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -5,7 +5,7 @@ INSERT INTO bears (name, age, gender, color, temperament, alive)
 VALUES ('Rowdy', 2, 'M', 'light brown', 'crazy', 1);
 
 INSERT INTO bears (name, age, gender, color, temperament, alive) 
-VALUES ('Tabitha', 15, 'F', 'white', 'calm', 1);
+VALUES ('Tabitha', 15, 'F', 'white', 'calm', 0);
 
 INSERT INTO bears (name, age, gender, color, temperament, alive) 
 VALUES ('Sergeant Brown', 12, 'M', 'black', 'angry', 1);
@@ -20,5 +20,5 @@ INSERT INTO bears (name, age, gender, color, temperament, alive)
 VALUES ('Wendy', 5, 'F', 'reddish', 'lazy', 1);
 
 INSERT INTO bears (name, age, gender, color, temperament, alive) 
-VALUES (NULL, 3, 'M', 'white', 'slayer', 1);
+VALUES (NULL, 3, 'M', 'white', 'slayer', 0);
 
