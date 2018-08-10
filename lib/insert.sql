788d541e6b4a7e2594b8118abc25df038ef1009a
Brenden Thornton <thorntonbrenden@gmail.com>
2018-08-10 21:52:07 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,24 +1,24 @@
 INSERT INTO bears (name, age, gender, color, temperament, alive) 
-VALUES ('Mr. Chocolate', 10, 'M', 'dark brown', 'smooth', 1)
+VALUES ('Mr. Chocolate', 10, 'M', 'dark brown', 'smooth', 1);
 
 INSERT INTO bears (name, age, gender, color, temperament, alive) 
-VALUES ('Rowdy', 2, 'M', 'light brown', 'crazy', 1)
+VALUES ('Rowdy', 2, 'M', 'light brown', 'crazy', 1);
 
 INSERT INTO bears (name, age, gender, color, temperament, alive) 
-VALUES ('Tabitha', 15, 'F', 'white', 'calm', 1)
+VALUES ('Tabitha', 15, 'F', 'white', 'calm', 1);
 
 INSERT INTO bears (name, age, gender, color, temperament, alive) 
-VALUES ('Sergeant Brown', 12, 'M', 'brown', 'angry', 1)
+VALUES ('Sergeant Brown', 12, 'M', 'brown', 'angry', 1);
 
 INSERT INTO bears (name, age, gender, color, temperament, alive) 
-VALUES ('Melissa', 8, 'F', 'red', 'adventerous', 1)
+VALUES ('Melissa', 8, 'F', 'red', 'adventerous', 1);
 
 INSERT INTO bears (name, age, gender, color, temperament, alive) 
-VALUES ('Grinch', 18, 'M', 'green', 'bitter', 1)
+VALUES ('Grinch', 18, 'M', 'green', 'bitter', 1);
 
 INSERT INTO bears (name, age, gender, color, temperament, alive) 
-VALUES ('Wendy', 5, 'F', 'reddish', 'lazy', 1)
+VALUES ('Wendy', 5, 'F', 'reddish', 'lazy', 1);
 
 INSERT INTO bears (name, age, gender, color, temperament, alive) 
-VALUES (NULL, 3, 'M', 'white', 'slayer', 1)
+VALUES (NULL, 3, 'M', 'white', 'slayer', 1);
 
