9bfe9eccfe57d5e8fa1d27baf92f43771fa8c9ba
Brenden Thornton <thorntonbrenden@gmail.com>
2018-08-10 21:48:01 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -5,4 +5,7 @@ INSERT INTO bears (name, age, gender, color, temperament, alive)
 VALUES ('Rowdy', 2, 'M', 'light brown', 'crazy', 1)
 
 INSERT INTO bears (name, age, gender, color, temperament, alive) 
+VALUES ('Tabitha', 15, 'F', 'white', 'calm', 1)
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) 
 VALUES ('Mr. Chocolate', 10, 'M', 'brown', 'smooth', 1)
